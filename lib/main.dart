34509886f7f1f32c0720e3e6be744bd1bc609c42
Ayush Bherwani <ayush.bherwani1998@gmail.com>
2019-12-03 21:03:27 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
 
